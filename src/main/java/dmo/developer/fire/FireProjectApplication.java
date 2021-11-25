8d90a62abca2a7db17c50168ee348f7d7646937e
DMO <dmo.developer@gmail.com>
2021-11-25 19:50:32 +0100

    Init project

---
+++ src/main/java/dmo/developer/fire/FireProjectApplication.java
@@ -0,0 +1,13 @@
+package dmo.developer.fire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmo/developer/fire/FireProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dmo.developer.fire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
